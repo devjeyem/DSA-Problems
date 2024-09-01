8b646dd722e4a848ecc88a308df1127e22414029
Jm <pintsjm@gmail.com>
2024-09-01 08:38:43 +0800

    Added Features

--- Codeforces/1A-TheatreSquare.c
+++ Codeforces/1A-TheatreSquare.c
@@ -2,5 +2,11 @@
 
 int main()
 {
-    
+    long long n,m,a;
+    scanf("%lld %lld %lld",&n,&m,&a);
+
+    long long  flagstonesInN = (n+a-1)/a;
+    long long flagstonesInM = (m+a-1)/a;
+    long long  flagstonesInNM = flagstonesInN*flagstonesInM;
+    printf("%lld\n",flagstonesInNM);
 }
