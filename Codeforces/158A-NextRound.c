0c8ed80f294bd873520b99e1c8066aefc325a64a
Jm <pintsjm@gmail.com>
2024-09-01 09:09:48 +0800

    Added Features

--- Codeforces/158A-NextRound.c
+++ Codeforces/158A-NextRound.c
@@ -2,5 +2,22 @@
 
 int main ()
 {
-    
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int ctr = 0;
+    int a[50];
+    for  (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int threshold = a[k - 1];
+    for (int i  = 0; i < n; i++)
+    {
+        if (a[i] >= threshold && a[i] > 0)
+        {
+            ctr++;
+        }
+    }
+    printf("%d\n", ctr);
 }
